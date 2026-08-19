be34c45b0f37ea73a3bf0c2a0746af196b780f78
João Dantas <joaorocha130709@gmail.com>
2026-08-18 21:01:09 -0300

    feat: adiciona selecao do modo de jogo

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 print("== JOGO DE XADREZ E DAMAS ==")
 
-jogador1 = input("Jogador 1: ")
-jogador2 = input("Jogador 2: ")
+print("=============================")
 
-print(f"Bem-vindos, {jogador1} e {jogador2}! Vamos começar o jogo.")
+print("Escolha o modo de jogo:")
+print("1 - PvP (Jogador vs Jogador)")
+print("2 - PvE (Jogador vs Computador)")
+
+modo_de_jogo = input("Digite sua escolha: ")
+
+print("=============================")
+
+if modo_de_jogo == "1":
+
+    print("Qual o nome dos jogadores?")
+
+    jogador1 = input("Jogador 1: ")
+    jogador2 = input("Jogador 2: ")
+
+    print(f"Bem-vindos, {jogador1} e {jogador2}! Vamos começar o jogo.")
+
+elif modo_de_jogo == "2":
+
+    print("Qual o nome do jogador?")
+
+    jogador1 = input("Jogador: ")
+    jogador2 = "Computador"
+
+    print(f"Bem-vindo, {jogador1}! Vamos começar o jogo.")
+
+else:
+    print("Opção inválida. Por favor, reinicie o jogo e escolha uma opção válida.")
